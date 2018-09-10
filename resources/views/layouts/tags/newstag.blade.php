f6e551f19b62b7041765ce9947afe7d789c9f3e3
nolepc <nolepc@example.com>
2018-09-10 18:32:43 +0200

    news reviews tag edited

--- resources/views/layouts/tags/newstag.blade.php
+++ resources/views/layouts/tags/newstag.blade.php
@@ -1,7 +1,7 @@
-<li><a href="{{url('/news/tags/Tesla')}}">Tesla</a></li>
+<li><a href="{{url('/news/tags/Mercedes')}}">Mercedes</a></li>
 <li><a href="{{url('/news/tags/Golf')}}">Golf</a></li>
 <li><a href="{{url('/news/tags/Porsche')}}">Porsche</a></li>
-<li><a href="{{url('/news/tags/Car show')}}">Car show</a></li>
-<li><a href="{{url('/news/tags/Carrera')}}">Carrera</a></li>
 <li><a href="{{url('/news/tags/BMW')}}">BMW</a></li>
+<li><a href="{{url('/news/tags/Carrera')}}">Carrera</a></li>
+<li><a href="{{url('/news/tags/Toyota')}}">Toyota</a></li>
 <li><a href="{{url('/news/tags/A8')}}">A8</a></li>
--- resources/views/layouts/tags/reviewstag.blade.php
+++ resources/views/layouts/tags/reviewstag.blade.php
@@ -1,7 +1,4 @@
 <li><a href="{{url('/reviews/tags/Tesla')}}">Tesla</a></li>
+<li><a href="{{url('/reviews/tags/BMW')}}">BMW</a></li>
+<li><a href="{{url('/reviews/tags/Jepp')}}">Jeep</a></li>
 <li><a href="{{url('/reviews/tags/Golf')}}">Golf</a></li>
-<li><a href="{{url('/reviews/tags/Porsche')}}">Porsche</a></li>
-<li><a href="{{url('/reviews/tags/Car show')}}">Car show</a></li>
-<li><a href="{{url('/reviews/tags/Carrera')}}">Carrera</a></li>
-<li><a href="{{url('/reviews/tags/M5')}}">M5</a></li>
-<li><a href="{{url('/reviews/tags/A8')}}">A8</a></li>
