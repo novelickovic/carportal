bc60b14a40b59d8e8943b6fb0f7e877becb2147a
nolepc <nolepc@example.com>
2018-09-10 18:34:51 +0200

    news reviews tag edited

--- resources/views/layouts/tags/newstag.blade.php
+++ resources/views/layouts/tags/newstag.blade.php
@@ -2,6 +2,6 @@
 <li><a href="{{url('/news/tags/Golf')}}">Golf</a></li>
 <li><a href="{{url('/news/tags/Porsche')}}">Porsche</a></li>
 <li><a href="{{url('/news/tags/BMW')}}">BMW</a></li>
-<li><a href="{{url('/news/tags/Carrera')}}">Carrera</a></li>
+<li><a href="{{url('/news/tags/Cayenne')}}">Cayenne</a></li>
 <li><a href="{{url('/news/tags/Toyota')}}">Toyota</a></li>
 <li><a href="{{url('/news/tags/A8')}}">A8</a></li>
--- resources/views/layouts/tags/reviewstag.blade.php
+++ resources/views/layouts/tags/reviewstag.blade.php
@@ -1,4 +1,4 @@
 <li><a href="{{url('/reviews/tags/Tesla')}}">Tesla</a></li>
 <li><a href="{{url('/reviews/tags/BMW')}}">BMW</a></li>
-<li><a href="{{url('/reviews/tags/Jepp')}}">Jeep</a></li>
+<li><a href="{{url('/reviews/tags/Jeep')}}">Jeep</a></li>
 <li><a href="{{url('/reviews/tags/Golf')}}">Golf</a></li>
